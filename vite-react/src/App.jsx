1986c68647b31d800a34da2d2b045f862531d2e9
dancinginthedarkk <91607846+dancinginthedarkk@users.noreply.github.com>
2024-01-15 18:51:32 +0300

    feat: add configuration vite + react (#5)

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export const App = () => {
+  return (
+    <>
+        <h1>Vite + React</h1>
+        <img src={reactLogo} alt="logo"/>
+    </>
+  )
+}
